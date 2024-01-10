a8a0a52fa8537730cd89a241ff9b87f2d391d788
Björn <bjorn.christensson@osynlig.se>
2024-01-10 15:13:07 +0100

    add Header and minor restructure

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import Header from "./Header/Header";
+
+const App = () => (
+  <>
+    <Header />
+    <main className="px-8 py-4">content</main>
+  </>
+);
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => (
+  <header className="bg-zinc-200 px-8 py-4 shadow-inner">
+    <h1 className="text-red-500 text-xl font-bold">Exchange Rate Calculator</h1>
+    <small>This is a barebone exchange rate calculator.</small>
+  </header>
+);
+
+export default Header;
