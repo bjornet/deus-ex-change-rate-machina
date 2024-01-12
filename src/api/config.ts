8bc20b8802c191008e206b91ba5c534ce848b375
Björn <bjorn.christensson@osynlig.se>
2024-01-12 13:42:36 +0100

    add some basic API config

---
+++ src/api/config.ts
@@ -0,0 +1,9 @@
+export const fetchOptionsGET = {
+  method: "GET",
+  headers: { accept: "application/json" },
+};
+
+/**
+ * @todo Read from env file instead
+ */
+export const oxrAPIBaseURL = "https://openexchangerates.org/api";
