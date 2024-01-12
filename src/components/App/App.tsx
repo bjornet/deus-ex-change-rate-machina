01ea95658cdff826cc372573bd26f9d3b00cc7a7
Björn <bjorn.christensson@osynlig.se>
2024-01-12 14:35:28 +0100

    uppdate App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,22 @@
+import { Exchange } from "components/Exchange";
+import { Header } from "components/Header";
+import { ExchangeRatesProvider } from "context/ExchangeRates";
+
+const App = () => (
+  <div
+    id="app-wrapper"
+    className="bg-slate-800 grid auto-rows-min h-screen text-slate-300"
+  >
+    <Header
+      title="Exchange Rate Determinator"
+      tag="Exchange rates made easy!"
+    />
+    <main className="px-8 py-4">
+      <ExchangeRatesProvider>
+        <Exchange />
+      </ExchangeRatesProvider>
+    </main>
+  </div>
+);
+
+export { App };
