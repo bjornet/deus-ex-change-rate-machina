7fc0a94c7cac912365bf11709695825b14b60a0b
Björn <bjorn.christensson@osynlig.se>
2024-01-12 14:30:45 +0100

    add Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,15 @@
-const Header = () => (
-  <header className="bg-zinc-200 px-8 py-4 shadow-inner">
-    <h1 className="text-red-500 text-xl font-bold">Exchange Rate Calculator</h1>
-    <small>This is a barebone exchange rate calculator.</small>
+import { FC } from "react";
+
+type HeaderProps = {
+  title: string;
+  tag: string;
+};
+
+const Header: FC<HeaderProps> = ({ title, tag }) => (
+  <header className="bg-slate-900 px-8 py-4 shadow-inner">
+    <h1 className="text-yellow-300 text-xl font-bold">{title}</h1>
+    <small className="text-yellow-100">{tag}</small>
   </header>
 );
 
-export default Header;
+export { Header };
