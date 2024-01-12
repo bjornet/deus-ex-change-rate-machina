4cbedae3ed1189b5257b9498efa8467c6295e655
Björn <bjorn.christensson@osynlig.se>
2024-01-12 14:28:06 +0100

    add Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,31 @@
+import { ChangeEventHandler, FC } from "react";
+
+type InputProps = {
+  id: string;
+  label: string;
+  value: string;
+  placeholder?: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+};
+
+const Input: FC<InputProps> = ({
+  id,
+  label,
+  value,
+  onChange,
+  placeholder = "",
+}) => (
+  <label className="block mb-2 font-bold" htmlFor={id}>
+    {label}
+    <input
+      className="block w-full p-2 border border-gray-300 rounded-md text-slate-600"
+      id={id}
+      type="text"
+      value={value || ""}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  </label>
+);
+
+export { Input };
