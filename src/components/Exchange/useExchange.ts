6a282b5349e3561b32cf4b982087a6f8fccbf9b8
Björn <bjorn.christensson@osynlig.se>
2024-01-13 13:58:03 +0100

    prune useExchange hook and add notes

--- src/components/Exchange/useExchange.ts
+++ src/components/Exchange/useExchange.ts
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
-import { Maybe } from "types/types";
+import { useState } from "react";
 import { storeManager } from "store";
-import { PersistedStateKeys } from "store/enums";
-import { useExchangeRates } from "context";
-import { getExchangeRate } from "./actions";
+import { StoreSegments } from "store/enums";
+
+/**
+ * @note I realize now that most of this state can and should be handled by the context layer.
+ * - it might be that I'm overusing the context layer relative to let the components manage more. This is a tradeoff
+ * and I'm far from sure what is the best balance.
+ * - I'm leaving it like this for now, but I would refactor this later and by doing so I expect less code and complexity. WIN!
+ */
 
 /**
  * @todo Move this to the config layer: config file or env variables
@@ -11,64 +15,29 @@ import { getExchangeRate } from "./actions";
 const SOURCE_CURRENCY_DEFAULT = "SEK";
 const TARGET_CURRENCY_DEFAULT = "NOK";
 
-export const useExchange = () => {
-  const { rates } = useExchangeRates();
+/**
+ * Restore exchange values on mount
+ */
+const storedSourceCurrency = storeManager.get(
+  StoreSegments.CURRENCY_SELECTED_SOURCE,
+);
 
-  const [exchangeRate, setExchangeRate] = useState<Maybe<number>>(null);
+const storedTargetCurrency = storeManager.get(
+  StoreSegments.CURRENCY_SELECTED_TARGET,
+);
+
+export const useExchange = () => {
   const [amount, setAmount] = useState<number>(1);
 
   const [sourceCurrencySelected, setCurrencySelected] = useState<string>(
-    SOURCE_CURRENCY_DEFAULT,
+    storedSourceCurrency || SOURCE_CURRENCY_DEFAULT,
   );
 
   const [targetCurrencySelected, setTargetCurrencySelected] = useState<string>(
-    TARGET_CURRENCY_DEFAULT,
+    storedTargetCurrency || TARGET_CURRENCY_DEFAULT,
   );
 
-  /**
-   * Update the exchange rate when the rates or the selected currencies change
-   */
-  useEffect(() => {
-    if (!rates) return;
-
-    const calculatedExchangeRate = getExchangeRate(
-      rates[sourceCurrencySelected],
-      rates[targetCurrencySelected],
-    );
-
-    setExchangeRate(calculatedExchangeRate);
-  }, [rates, sourceCurrencySelected, targetCurrencySelected]);
-
-  /**
-   * Restore exchange values on mount
-   * @note this should extracted to a custom hook
-   */
-  useEffect(() => {
-    const storedAmount = storeManager.get(PersistedStateKeys.AMOUNT);
-
-    const storedSourceCurrency = storeManager.get(
-      PersistedStateKeys.CURRENCY_SELECTED_SOURCE,
-    );
-
-    const storedTargetCurrency = storeManager.get(
-      PersistedStateKeys.CURRENCY_SELECTED_TARGET,
-    );
-
-    if (storedAmount) {
-      setAmount(storedAmount);
-    }
-
-    if (storedSourceCurrency) {
-      setCurrencySelected(storedSourceCurrency);
-    }
-
-    if (storedTargetCurrency) {
-      setTargetCurrencySelected(storedTargetCurrency);
-    }
-  }, [exchangeRate]);
-
   return {
-    exchangeRate,
     amount,
     setAmount,
     sourceCurrencySelected,
