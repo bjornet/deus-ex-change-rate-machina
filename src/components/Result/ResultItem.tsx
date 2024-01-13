6d00f5e24612b22450b85828c2a331a9118a4e5d
Björn <bjorn.christensson@osynlig.se>
2024-01-13 13:58:59 +0100

    add ResultItem component

---
+++ src/components/Result/ResultItem.tsx
@@ -0,0 +1,53 @@
+import { Pill } from "components/Pill";
+import { TARGET_DECIMALS_PRECISION } from "context/ExchangeRates/ExchangeRates";
+import { FC } from "react";
+import { localizeNumber } from "utils";
+import { ResultItemClose } from "./ResultItemClose";
+
+type ResultItemProps = {
+  amount: number;
+  currency: string;
+  exchangeRate: number;
+  onClick: (currency: string) => void;
+};
+
+const ResultItem: FC<ResultItemProps> = ({
+  amount,
+  currency,
+  exchangeRate,
+  onClick,
+}) => {
+  const handleClick = () => {
+    onClick(currency);
+  };
+
+  const targetValue = amount * exchangeRate;
+
+  return (
+    <div className="text-4xl mt-2 p-4 border-2 border-slate-200 rounded-md relative min-h-10 box-content">
+      {targetValue ? (
+        <>
+          <ResultItemClose onClick={handleClick} />
+          <div>
+            ={" "}
+            {localizeNumber(
+              "sv",
+              currency,
+              targetValue,
+              "decimal",
+              TARGET_DECIMALS_PRECISION,
+            )}{" "}
+            <Pill color="blue" value={currency} />
+          </div>
+        </>
+      ) : (
+        <>
+          <ResultItemClose onClick={handleClick} />
+          <div>N/A</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export { ResultItem };
